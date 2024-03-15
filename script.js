ef7ad3b6dec9fc396028935b648105b4cd3cadf0
Aimona <aimonadunaeva@gmail.com>
2024-03-15 23:55:14 +0600

    final commit

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ let userData = {
 
 userData.age = prompt("напишите свой возвраст: ", 18);
 
-userData.underage = confirm("вы совершеннолетний ?");
+userData.underage = confirm("вы несовершеннолетний ?");
 
-userData.adress = {
-  country: "Kyrgyzstan",
-  city: "Osh",
-  street: "manas prospect",
+let keyToDelete = prompt(
+  "Введите ключ объекта, который хотите удалить",
+  "name или age"
+);
+delete userData[keyToDelete];
+
+userData.address = {
+  country: prompt("Введите вашу страну:"),
+  city: prompt("Введите ваш город:"),
+  street: prompt("Введите вашу улицу:"),
 };
 
 console.log(userData);
